a0d40a7819195628ae012ed74cf5cc9123b7a6d9
mikebono1 <tdfbands@gmail.com>
2021-02-20 14:26:04 -0500

    Added a default error page and error handling function, a database for the providers patient info, and some scripts to create table of patient data, and some misc. navigation buttons

--- scripts.js
+++ scripts.js
@@ -43,3 +43,39 @@ function authenticateLogin(validate){
         document.getElementById("authenticationErrors").innerHTML = "Invalid Authorization Code: " + validate;
     }
 }
+
+//incomplete function, once Mike learns php he will complete this function and it will replace above authenticateLogin function
+function passwordProtection (validationCode){
+    //Validation code of 1 = provider, 2 = patient, 3 = admin
+    var username, password;
+    if(validationCode ==  1){
+        
+    }
+    if(validationCode == 2){
+
+    }
+    if(validationCode == 3){
+
+    }
+    if(validationCode > 3){
+
+    }
+}
+
+//collects stored patient data files and converts it to html data displayed by the provider_database.html table
+function displayPatientRecord(){
+    var patient1 = ["John Doe", "Male", "46", "5'11&quot", "165"];
+    var patient2 = []; //eventually replace with php script to get data from sql server
+    var txt = "<tr>"
+    var i;
+    //patient arrays will be replaced by php database reading code to avoid manual entry
+    for(i = 0; i < patient1.length; i++){
+        txt = txt + "<td>" + patient1[i] + " </td>"; //cycles trhough each array element and makes each a table cell
+    }
+    txt = txt + "</tr>"
+    document.getElementById("patientDatabase").innerHTML = txt;
+}
+
+function displayError(errorCode){
+    
+}
