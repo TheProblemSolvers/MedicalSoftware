d6e961d00eb4cd14e3a9f93cd947899bea3a90dd
Matthew Heras <matthewheras1@gmail.com>
2021-02-28 21:07:04 -0500

    fixed tableCreate function

--- scripts.js
+++ scripts.js
@@ -59,20 +59,21 @@ function displayPatientRecord(){
 function tableCreate(rows, cols, cellData) {
     var body = document.getElementsByTagName('body')[0];
     var tbl = document.createElement('table');
+    var tbdy = document.createElement('tbody');
     tbl.style.width = '100%';
     tbl.setAttribute('border', '1');
-    var tbdy = document.createElement('tbody');
     for (var i = 0; i < rows; i++) {
       var tr = document.createElement('tr');
       for (var j = 0; j < cols; j++) {
         if (i == rows && j == cols) {
-          break
+          break;
         } else {
           var td = document.createElement('td');
           td.appendChild(document.createTextNode(cellData))
           tr.appendChild(td)
         }
       }
+      tbdy.appendChild(tr);
     }
     tbl.appendChild(tbdy);
     body.appendChild(tbl);
