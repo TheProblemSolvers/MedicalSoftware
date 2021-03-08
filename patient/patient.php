accf54c1f301df48176526211fc4fbe4a51a0e6b
mikebono1 <tdfbands@gmail.com>
2021-03-07 19:24:56 -0500

    some bug fixes and streamlining of PHP code

---
+++ patient/patient.php
@@ -0,0 +1,6 @@
+<?php
+
+#adds the user's name to the ready list on the checkin.txt file
+function patientArrived($userId){
+    
+}
--- test/test.php
+++ test/test.php
@@ -1,5 +1,9 @@
 <?php
 
 function testFunction(){
-    
+    $lineContents = "p12938469369.FirstName=Patient hurt his knee while jumping on a track.";
+    $startRead = strpos($lineContents, ".") + 1;
+    $endRead = strpos($lineContents, "=");
+    return substr($lineContents, $startRead, ($endRead - $startRead));
+    //return $endRead;
 }
