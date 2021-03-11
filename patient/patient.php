254ae3c87c47a7ce1d49bca2a53453743fbe039c
mikebono1 <tdfbands@gmail.com>
2021-03-11 08:08:24 -0500

    added an account sync feature for the patient, and another feature that allows them to access their data that their provider has on file

--- patient/patient.php
+++ patient/patient.php
@@ -1,9 +1,9 @@
 <?php
 
 #accesses correct user database
-function accessUserDatabase($userId){
+function accessUserDatabase($userId, $openType){
     $fileName = "../users\user_data\#" . $userId . ".txt";
-    $fileHandle = fopen($fileName, "r");
+    $fileHandle = fopen($fileName, $openType);
     return $fileHandle;
 }
 
@@ -41,9 +41,57 @@ function patientFullName($userId, $patientId){
     }
 }
 
-#Allows patient and provider to sync their accounts
-function accountSync($userId, $linkId){
-    
+#Allows patient to sync their account to their provider
+function patientToProvider($userId, $linkId){
+    $fileHandle = accessUserDatabase($userId, "a");
+    $linkData = "ProviderAccount=" . strval($linkId) . "\n";
+    fwrite($fileHandle, $linkData);
+    fclose($fileHandle);
+    return true;
+}
+
+#generates an html table with only patient's data
+function generatePatientTable($userId){
+    $fileHandle = accessUserDatabase($userId, "r");
+
+    $htmlTable = "<br><table><thead><tr><th>First Name</th><th>Last Name</th><th>Patient Notes</th></tr></thead><tbody><tr>";
+    $htmlEndTable = "</tr></tbody></table>";
+    $providerId = null;
+
+    #filters through patient's file and finds linked provider's account
+    while(feof($fileHandle) == false){
+        $lineContents = fgets($fileHandle);
+        if(preg_match("/ProviderAccount/i", $lineContents) == 1){
+            
+            $startRead = strpos($lineContents, "=") + 1;
+            $providerId = trim(substr($lineContents, $startRead));
+            break;
+        }
+    }
+    fclose($fileHandle);
+
+    #if there was no linked provider account, return an error
+    if($providerId == null){
+        return "No linked provider account.";
+    }
+
+    #opens the provider's file and finds the patient's information
+    $fileHandle = accessUserDatabase($providerId, "r");
+
+    #filters through provider's file and lands on patient's info
+    while($userId != getPatientId($lineContents)){
+        $lineContents = fgets($fileHandle);
+    }
+    #compiles all of selected patient's data into a table to display
+    while($userId == getPatientId($lineContents)){
+        $startRead = strpos($lineContents, "=") + 1;
+        $htmlTable = $htmlTable . "<td>" . substr($lineContents, $startRead) . "</td>";
+        $lineContents = fgets($fileHandle);
+    }
+    $htmlTable = $htmlTable . $htmlEndTable;
+
+    fclose($fileHandle);
+    return $htmlTable;
 }
 
 #adds the user's name to the ready list on the checkin.txt file
