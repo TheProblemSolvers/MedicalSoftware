28979a93404b7ffaa287750abdd867510f749376
Matthew Heras <matthewheras1@gmail.com>
2021-04-28 18:08:40 -0400

    Fixed calendar to cycle through months and fixed discoloration issues

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 //sets a cookie for a page based on what link is clicked
 function setPatientIdCookie(id){
   document.cookie = "patientId=" + id;
@@ -97,40 +98,56 @@ function dateRec()  {
 
 }
 
-function appleBottomJeans() {
+//function for modifying an attribute of all elements in a class
 
-  // const cloneTables = document.getElementsByClassName('random_test');
+  const cloneTables = document.getElementsByClassName('random_test');
   
-  // Array.from(cloneTables).forEach(function (cloneTable) {
+  Array.from(cloneTables).forEach(function (cloneTable) {
 
-  //     cloneTable.style.transform = 'translate(-50%)';
+      cloneTable.style.transform = 'translate(-50%)';
 
-  //   });
+    });
 
-  document.getElementById('RandomTest1').style.transform = 'translateX(-1920px)';
-
-}
-
-// function blackTieAffair () {
-
-//   document.getElementById('baseCal').style.transform = 'translateX(1250px)';
-
-// }
+//function for cycling to the next month
 
 function nextMonth () {
 
   currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-  currentMonth = (currentMonth + 1) % 12;
-  printCalendar(currentMonth, currentYear, 0);
 
+  currentMonth = (currentMonth + 1) % 12;
+
+  printCalendar(currentMonth, currentYear);
 
 }
+
+//function for cycling to the previous month
 
 function previousMonth () {
 
+  currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+
+  currentMonth = (currentMonth === 0) ? 11 : (currentMonth - 1) % 12;
+
+  printCalendar(currentMonth, currentYear);
+
 }
 
+//function to remove a month from the page altogther
+
+function removeTable() {
+
+  var affTable = document.getElementById('Id1');
+
+  var parentEl = affTable.parentElement;
+
+  parentEl.removeChild(affTable);
+
+}
+
+
+
 //Global declarations for the printCalendar function
+
 let today = new Date();
 let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
@@ -142,97 +159,158 @@ let months = ["January", "February", "March", "April", "May", "June", "July", "A
 let monthAndYear = document.getElementById("monthAndYear");
 
 
-//Function that creates each different month calendar page
-function printCalendar(month, year, pixel) {
+//Function that creates calendar
+
+function printCalendar(month, year) {
 
   //Declarations of month data for later use
+
+
   let firstDay = (new Date(year, month)).getDay();
+
   let daysInMonth = 32 - new Date(year, month, 32).getDate();
+
   var monthName=months[month];
 
   //Creation of the body, table, table body and top row for headers
+
   var body = document.getElementsByTagName('body')[0];
+
   var tbl = document.createElement('table');
-  tbl.id = 'RandomTest1';
-  // tbl.className = "random_test";
-  tbl.style.transform = `translateX(${pixel}px)`;
+
+  tbl.id = "Id1";
+
   var tbdy = document.createElement('tbody');
 
   var headerRow = document.createElement('tr');
 
-  //Left button with translation functionality
+  //Left button
+
     var theader = document.createElement('th');
     theader.id = "arrowLeftHeader";
 
       var btn1 = document.createElement('button');
-        btn1.id = "arrowLeft";
-        btn1.onclick = function() {appleBottomJeans(); nextMonth();y};
-          var iElement = document.createElement('i');
-          iElement.setAttribute('class', 'arrow left');
-        btn1.appendChild(iElement);
-      theader.appendChild(btn1);
+      btn1.id = "arrowLeft";
+
+      btn1.onclick = function() {
+
+        removeTable();
+        previousMonth();
+
+      };
+
+      var iElement = document.createElement('i');
+      iElement.setAttribute('class', 'arrow left');
+
+      btn1.appendChild(iElement);
+    theader.appendChild(btn1);
 
   //Center header w/ month name
+
   var theader2 = document.createElement('th');
+
   theader2.setAttribute('colspan', '5')
+
   theader2.appendChild(document.createTextNode(monthName));
 
-  //Right button with translation functionality
+  //Right button
+
     var theader3 = document.createElement('th');
     theader3.id = "arrowRightHeader";
 
       var btn2 = document.createElement('button');
-        btn2.id = "arrowRight";
-          btn2.onclick = function() {blackTieAffair(); /*printCalendar(currentMonth + 1, currentYear)*/};
-          var iElement2 = document.createElement('i');
-          iElement2.setAttribute('class', 'arrow right');
-        btn2.appendChild(iElement2);
-      theader3.appendChild(btn2);
+      btn2.id = "arrowRight";
+
+      btn2.onclick = function() {
+        
+        removeTable();
+        nextMonth();
+
+      };
+
+      var iElement2 = document.createElement('i');
+      iElement2.setAttribute('class', 'arrow right');
+
+      btn2.appendChild(iElement2);
+    theader3.appendChild(btn2);
 
   //Appending all of the sub headers to the main header row
+
   headerRow.appendChild(theader);
   headerRow.appendChild(theader2);
   headerRow.appendChild(theader3);
 
   //Appending the main header row to the body of the table
+
   tbdy.appendChild(headerRow);
 
   // creating all cells
+
   let date = 1;
+
   for (let i = 0; i < 6; i++) {
+
       // creates a table row
+
       let row = document.createElement("tr");
 
       //creating individual cells, filing them up with data.
+
       for (let j = 0; j < 7; j++) {
 
         //Adds the blank spaces before the actual dates start appearing
+
           if (i === 0 && j < firstDay) {
+
               let cell = document.createElement("td");
               let cellText = document.createTextNode("");
               cell.appendChild(cellText);
               row.appendChild(cell);
+
           }
 
           //When the current date is greater than the days in the month the function stops
+
           else if (date > daysInMonth) {
+
               break;
+
           }
 
           else {
-              let cell = document.createElement("td");
-              let cellText = document.createTextNode(date);
-              cell.appendChild(cellText);
-              row.appendChild(cell);
-              date++;
+
+            let cell = document.createElement("td");
+
+            var container = document.createElement("span");
+
+            let cellText = document.createTextNode(date);
+
+              if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+
+                container.style.color = "magenta";
+
+              }
+
+            container.appendChild(cellText);
+
+            cell.appendChild(container);
+
+            row.appendChild(cell);
+            
+            date++;
 
               //Adds the blank spaces after the dates to fill the calendar completely
-              if (date > daysInMonth && j < 7) {
+
+          if (date > daysInMonth && j < 7) {
+
                 for (let z = 0; z < 6 - j; z++) {
 
                   let cell = document.createElement("td");
+
                   let cellText = document.createTextNode("");
+
                   cell.appendChild(cellText);
+
                   row.appendChild(cell);
 
                 }
@@ -244,11 +322,15 @@ function printCalendar(month, year, pixel) {
       }
 
       //Appending the rows into the calendar
+
       tbdy.appendChild(row);
+
   }
 
   //Appending the table body to the table and the table to the body
+
   tbl.appendChild(tbdy);
+  
   body.appendChild(tbl);
 
 }
